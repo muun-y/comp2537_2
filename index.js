05cd1558c9e5a86865470c3b4ada4b53957bfcdf
Munyoung Cho <munyoungc31@gmail.com>
2023-05-07 19:54:28 -0700

    edit the code

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("./utils.js");
+
 require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
@@ -7,35 +8,27 @@ const bcrypt = require("bcrypt");
 const saltRounds = 12;
 
 const port = process.env.PORT || 3000;
-
 const app = express();
-
 const Joi = require("joi");
 
-//Expires after 1 hour (hour * minutes * seconds * milliseconds)
 const expireTime = 1 * 60 * 60 * 1000;
 
-//Users and Passwords (in memory 'database')
-var users = [];
-
-/* Secret Information Section */
+/* secret information section */
 const mongodb_host = process.env.MONGODB_HOST;
 const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
 const mongodb_database = process.env.MONGODB_DATABASE;
 const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
-
 const node_session_secret = process.env.NODE_SESSION_SECRET;
 /* END secret section */
 
 var { database } = include("databaseConnection");
 
+app.set("view engine", "ejs");
+
 const userCollection = database.db(mongodb_database).collection("users");
 
-app.set('view engine', 'ejs'); 
-
-//middleware get a chance to run 
-app.use(express.urlencoded({ extended: false })); 
+app.use(express.urlencoded({ extended: false }));
 
 var mongoStore = MongoStore.create({
   mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
@@ -44,81 +37,81 @@ var mongoStore = MongoStore.create({
   },
 });
 
-//parsing out the cookies 
 app.use(
   session({
     secret: node_session_secret,
-    store: mongoStore, 
+    store: mongoStore, //default is memory store
     saveUninitialized: false,
     resave: true,
   })
 );
 
-function isValidSession(req){
-  if(req.session.authenticated){
-    return true; 
+function isValidSession(req) {
+  if (req.session.authenticated) {
+    return true;
   }
-  return false; 
+  return false;
 }
 
-//middleware function
-function sessionValidation(req,res,next){
-  if(isValidSession(req)){
-    next(); 
-  }
-  else{
-    res.redirect('/login'); 
+function sessionValidation(req, res, next) {
+  if (isValidSession(req)) {
+    next();
+  } else {
+    res.redirect("/login");
   }
 }
 
 function isAdmin(req) {
-  if (req.session.user_type == 'admin') {
-      return true;
+  if (req.session.user_type == "admin") {
+    return true;
   }
   return false;
 }
 
 function adminAuthorization(req, res, next) {
   if (!isAdmin(req)) {
-      res.status(403);
-      res.render("errorMessage", {error: "Not Authorized"});
-      return;
-  }
-  else {
-      next();
+    var isAuthenticated = req.session.authenticated || false;
+    res.status(403);
+    res.render("403", {
+      authenticated: isAuthenticated,
+      error: "Not Authorized - 403",
+    });
+    return;
+  } else {
+    next();
   }
 }
 
-//Home page that checks for authentication status and offers login or sign-up
 app.get("/", (req, res) => {
-  if (!req.session.authenticated) {
-   res.render("index"); 
+  var isAuthenticated = req.session.authenticated || false;
+  if (!isAuthenticated) {
+    res.render("main", { authenticated: isAuthenticated });
+    return;
   } else {
-    res.redirect('/loggedin'); 
+    res.render("index", {
+      authenticated: isAuthenticated,
+      username: req.session.username,
+    });
   }
 });
 
-app.get("/nosql-injection", async (req, res) => {
-  var name = req.query.user;
+app.get("/nosql-injection", sessionValidation, async (req, res) => {
+  var username = req.query.user;
 
-  if (!name) {
+  if (!username) {
     res.send(
-      `<h3>No user provided - try /nosql-injection?user=name</h3> <h3>or /nosql-injection?user[$ne]=name</h3>`
+      `<h3>no user provided - try /nosql-injection?user=name</h3> <h3>or /nosql-injection?user[$ne]=name</h3>`
     );
     return;
   }
-  console.log("user: " + name);
+  console.log("user: " + username);
 
   const schema = Joi.string().max(20).required();
-  const validationResult = schema.validate(name);
+  const validationResult = schema.validate(username);
 
-  // If we didn't use Joi to validate and check for a valid URL parameter below
-  // we could run our userCollection.find and it would be possible to attack.
-  // A URL parameter of user[$ne]=name would get executed as a MongoDB command
-  // and may result in revealing information about all users or a successful
-  // login without knowing the correct password.
   if (validationResult.error != null) {
     console.log(validationResult.error);
+    res.redirect("/login");
     res.send(
       "<h1 style='color:darkred;'>A NoSQL injection attack was detected!!</h1>"
     );
@@ -126,50 +119,60 @@ app.get("/nosql-injection", async (req, res) => {
   }
 
   const result = await userCollection
-    .find({ name: name })
-    .project({ name: 1, password: 1, _id: 1 })
+    .find({ email: email })
+    .project({ email: 1, password: 1, username: 1, _id: 1 })
     .toArray();
 
   console.log(result);
 
-  res.send(`<h1>Hello, ${name}!</h1>`);
+  res.send(`<h1>Hello ${username}</h1>`);
 });
 
-app.get('/createUser', (req,res) => {
-  res.render("createUser");
+app.get("/signup", (req, res) => {
+  var isAuthenticated = req.session.authenticated || false;
+  res.render("signup", { authenticated: isAuthenticated });
 });
 
 app.get("/login", (req, res) => {
-  res.render("login");
+  var isAuthenticated = req.session.authenticated;
+  res.render("login", { authenticated: isAuthenticated });
 });
 
-app.post('/submitUser', async (req,res) => {
+app.post("/submitUser", async (req, res) => {
   var username = req.body.username;
+  var email = req.body.email;
   var password = req.body.password;
 
-const schema = Joi.object(
-  {
+  const schema = Joi.object({
     username: Joi.string().alphanum().max(20).required(),
-    password: Joi.string().max(20).required()
+    password: Joi.string().max(20).required(),
+    email: Joi.string().email().required(),
   });
 
-const validationResult = schema.validate({username, password});
-if (validationResult.error != null) {
-   console.log(validationResult.error);
-   res.redirect("/createUser");
-   return;
- }
+  const validationResult = schema.validate({ email, username, password });
+  if (validationResult.error != null) {
+    console.log(validationResult.error);
+    res.render("errorMessage", { error: `${validationResult.error.message}`, authenticated: req.session.authenticated });
+    return;
+  }
 
   var hashedPassword = await bcrypt.hash(password, saltRounds);
 
-await userCollection.insertOne({username: username, password: hashedPassword, user_type: "user"});
-console.log("Inserted user");
+  await userCollection.insertOne({
+    username: username,
+    password: hashedPassword,
+    email: email,
+    user_type: "user",
+  });
 
-  var html = "successfully created user";
-  res.render("submitUser", {html: html});
+  req.session.authenticated = true;
+  req.session.username = username;
+  req.session.cookie.maxAge = expireTime;
+
+  res.redirect("/members");
 });
 
-app.post("/loggingin", async (req, res) => {
+app.post("/loggingIn", async (req, res) => {
   var email = req.body.email;
   var password = req.body.password;
 
@@ -177,113 +180,113 @@ app.post("/loggingin", async (req, res) => {
   const validationResult = schema.validate(email);
   if (validationResult.error != null) {
     console.log(validationResult.error);
-    res.redirect("/login");
+    res.render("errorMessage", { error: `${validationResult.error.message}`, authenticated: req.session.authenticated });
     return;
   }
 
   const result = await userCollection
     .find({ email: email })
-    .project({ name: 1, email: 1, password: 1, _id: 1 })
+    .project({ username: 1, email: 1, password: 1, user_type: 1, _id: 1 })
     .toArray();
 
   console.log(result);
   if (result.length != 1) {
-    console.log("User not found");
-    res.send(`Invalid email/password combination. Please <a href="/login">try again</a>.`);
+    res.render("errorMessage", { 
+      error: "User does not exist",
+      authenticated: req.session.authenticated,
+    });
     return;
   }
   if (await bcrypt.compare(password, result[0].password)) {
-    console.log("Correct Password");
     req.session.authenticated = true;
     req.session.email = email;
-    req.session.name = result[0].name;
+    req.session.username = result[0].username;
+    req.session.user_type = result[0].user_type;
     req.session.cookie.maxAge = expireTime;
 
-    res.redirect("/loggedin");
+    res.redirect("/members");
     return;
-  } 
-  else {
-    res.send(`Invalid email/password combination. Please <a href="/login">try again</a>.`);
-    res.redirect('/login'); 
-    return;
+  } else {
+    res.render("errorMessage", {
+      error: "Incorrect email/password combination",
+      authenticated: req.session.authenticated,
+    });    
+    return;    
   }
 });
 
-app.use("/loggedin", sessionValidation); 
-app.get("/loggedin", (req, res) => {
-  if (!req.session.authenticated) {
-    res.redirect('/login');
-}
-res.render("loggedin");
-  });
-  
-//   app.get('/loggedin/info', (req,res) => {
-//     res.render("loggedin-info");
-// });
 
-  app.get("/logout", (req, res) => {
-    req.session.destroy();
-    res.render("loggedout");
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
 });
 
-app.get('/cat/:id', (req,res) => {
-  var cat = req.params.id; 
-  res.render("cats", {cat: cat}); 
-}); 
+app.get("/members", (req, res) => {
+  var isAuthenticated = req.session.authenticated;
+  if (!isAuthenticated) {
+    res.redirect("login");
+  } else {
+    var username = req.session.username;
+    res.render("members", {
+      authenticated: isAuthenticated,
+      username: username,
+    });
+  }
+});
 
-//     const imageURL = [
-//     "Cookie.gif",
-//     "Cute.gif",
-//     "fluffy.gif",
-//     "Yay.gif"
-//   ];
+app.get("/admin", sessionValidation, adminAuthorization, async (req, res) => {
+  const result = await userCollection
+    .find()
+    .project({ username: 1, user_type: 1 })
+    .toArray();
+  var isAuthenticated = req.session.authenticated || false;
+  res.render("admin", {
+    users: result,
+    username: req.session.username,
+    authenticated: isAuthenticated,
+  });
+});
 
-//   app.get("/image/:id", (req, res) => {
-//   var meme = req.params.id;
+app.get("/promote/:username", async (req, res) => {
+  var username = req.params.username;
+  try {
+    await userCollection.findOneAndUpdate(
+      { username: username },
+      { $set: { user_type: "admin" } }
+    );
+    res.redirect("/admin");
+  } catch (err) {
+    console.log(err);
+    res.send("Error promoting user");
+  }
+});
 
-//   if (meme == 1) {
-//     res.send(`<img src='/${imageURL[0]}'>`);
-//   } else if (meme == 2) {
-//     res.send(`<img src='/${imageURL[1]}'>`);
-//   } else if (meme == 3) {
-//     res.send(`<img src='/${imageURL[2]}'>`);
-//   } else {
-//     res.send(`<img src='/${imageURL[3]}'>`);
-//   }
-// });
-
-
-//   app.get("/members", (req, res) => {
-//     if (!req.session.name) {
-//       res.redirect("/login");
-//       return;
-//     }
-  
-//     const name = req.session.name;
-//     const image = imageURL[Math.floor(Math.random() * imageURL.length)];
-  
-//     const html = `
-//       <h1>Hello, ${name}!</h1>
-//       <img src="/${image}" alt="Random image">
-//       <br><br>
-//       <button onclick="window.location.href='/logout'">Log out</button>
-//     `;
-//     res.send(html);
-//   });
-
-  app.get('/admin', sessionValidation, adminAuthorization, async (req,res) => {
-    const result = await userCollection.find().project({username: 1, _id: 1}).toArray();
- 
-    res.render("admin", {users: result});
+app.get("/demote/:username", async (req, res) => {
+  var username = req.params.username;
+  try {
+    await userCollection.findOneAndUpdate(
+      { username: username },
+      { $set: { user_type: "user" } }
+    );
+    res.redirect("/admin");
+  } catch (err) {
+    console.log(err);
+    res.send("Error promoting user");
+  }
 });
 
 app.use(express.static(__dirname + "/public"));
 
 app.get("*", (req, res) => {
   res.status(404);
-  res.render("404"); 
+  var isAuthenticated = req.session.authenticated || false;
+  res.render("404", {
+    authenticated: isAuthenticated,
+    error: "Page not found",
+  });
+  return;
 });
 
 app.listen(port, () => {
-  console.log("Assignment 2 listening on port " + port);
+  console.log("Node application listening on port " + port);
 });
